41afe47df3aa6e8fbbd72384297b56b41330dd33
shumpei0601 <s.urayama0601@gmail.com>
2022-04-02 17:55:15 +0900

    message

--- database/seeders/AreasTableSeeder.php
+++ database/seeders/AreasTableSeeder.php
@@ -16,16 +16,19 @@ class AreasTableSeeder extends Seeder
     public function run()
     {
         $param = [
+            'id' => 1,
             'name' => '東京都'
         ];
         DB::table('areas')->insert($param);
 
         $param = [
+            'id' => 2,
             'name' => '大阪府'
         ];
         DB::table('areas')->insert($param);
 
         $param = [
+            'id' => 3,
             'name' => '福岡県'
         ];
         DB::table('areas')->insert($param);
