2fb420897651f2c7e2e8fe77793d7644f83e1be2
shumpei0601 <s.urayama0601@gmail.com>
2022-04-03 10:23:11 +0900

    message

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,20 +1,11 @@
-<!doctype html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/reset.css" />
-    <link rel="stylesheet" href="css/nav.css" />
+@extends('layouts.default')
+
+@section('head')
     <script src="https://kit.fontawesome.com/d5b4d4baf0.js" crossorigin="anonymous"></script>
-</head>
+@endsection
 
 <x-guest-layout>
-
-<body>
-
+@section('content')
     <div class="ttl">
         @auth
             <nav class="nav" id="nav">
@@ -95,10 +86,10 @@
             </div>
         </div>
     </x-auth-card>
+    @endsection
 
-<script src="/js/main.js"></script>
-</body>
+    @section('scripts')
+    @endsection
 
 </x-guest-layout>
-</html>
 
