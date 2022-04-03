1dfdd5ac3d04621aa74fcf82aa265ae6eb4403db
shumpei0601 <s.urayama0601@gmail.com>
2022-04-03 11:13:00 +0900

    message

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -19,7 +19,7 @@ class LikeController extends Controller
   }
   public function delete(Request $request)
   {
-      $like = Like::where('shop_id', $request->shop_id)->where('user_id', Auth::id())->delete();
+      $like = Like::where('shop_id', $request->shop_id)->delete();
       return back()->with('like');
   }
 }
