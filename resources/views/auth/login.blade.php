c2892d9a8569cebe6f1d543c13026d9637b46166
shumpei0601 <s.urayama0601@gmail.com>
2022-04-03 11:54:59 +0900

    message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,10 +42,6 @@
                         <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" placeholder="Password"/>
                     </div>
 
-                    <!-- Remember Me -->
-                    <div class="block mt-4">
-                    </div>
-
                     <div class="flex items-center justify-end mt-4">
                         @if (Route::has('password.request'))
                             <a class="underline text-sm text-gray-600 hover:text-gray-900"
