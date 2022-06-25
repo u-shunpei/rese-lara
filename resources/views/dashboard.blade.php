ab2299a4eba153f781af07086f968508aa791d17
shumpei0601 <s.urayama0601@gmail.com>
2022-06-25 12:33:34 +0900

    message

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
     <div class="card">
         <p>ログインしました</p>
         <button class="home_btn">
-            <a href="https://restaurant-pj.herokuapp.com/" class="home">Homeへ</a>
+            <a href="https://rese-larapj.herokuapp.com/" class="home">Homeへ</a>
 {{--            <a href="http://localhost:8000/">Homeへ</a>--}}
         </button>
     </div>
